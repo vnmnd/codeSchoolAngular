467fa722a5596f2677659a16539848ca6024e49e
Garrett Sand <garrettsand@gmail.com>
2015-10-12 15:56:21 -0700

    Finish chapter1

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-var app = angular.module('store', [ ]);
+(function(){
+  var app = angular.module('store', [ ]);
+
+  app.controller('StoreController', function(){
+    this.products = gems;
+  });
+
+  var gems = [
+    {
+      name: 'Dodecahedron',
+      price: 2.95,
+      description: '. . .',
+      canPurchase: true,
+      soldOut: false
+    },
+    {
+      name: "Pentagonal Gem",
+      price: 5.95,
+      description: ". . .",
+      canPurchase: false,
+    }
+  ];  
+})();
