e2a0d24cdc5d054fc5f4804a23aae8d567eb871d
Garrett Sand <garrettsand@gmail.com>
2015-10-14 16:53:16 -0700

    Finish Chapter 2

--- app.js
+++ app.js
@@ -2,7 +2,18 @@
   var app = angular.module('store', [ ]);
 
   app.controller('StoreController', function(){
-    this.products = gems;
+    this.products = gems; 
+  });
+
+  app.controller("PanelController", function(){
+    this.tab = 1;
+
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+    this.isSelected = function(checkTab){
+      return this.tab === checkTab;
+    }
   });
 
   var gems = [
@@ -11,13 +22,21 @@
       price: 2.95,
       description: '. . .',
       canPurchase: true,
-      soldOut: false
+      soldOut: false,
+      images: [
+        "images/gem-01.gif",
+        "images/gem-02.gif"
+      ]
     },
     {
       name: "Pentagonal Gem",
       price: 5.95,
       description: ". . .",
       canPurchase: false,
+      images: [
+        "images/gem-03.gif",
+        "images/gem-04.gif"
+      ]
     }
   ];  
 })();
